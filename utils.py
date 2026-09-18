bcdde48223468a646c2e1324b040d9836b55c7c8
Kareem Adly <kareemadly45@gmail.com>
2026-09-18 21:51:04 +0300

    utils.py

--- utils.py
+++ utils.py
@@ -38,7 +38,8 @@ except Exception:
 # ============================================
 @st.cache_resource
 def get_supabase():
-    url = st.secrets["SUPABASE_URL"]
+    # الرابط مكتوب بالكامل بحروف إنجليزية (Latin)
+    url = "https://scpaqujqzckxuuyibsze.supabase.co"
     key = st.secrets["SUPABASE_KEY"]
     return create_client(url, key)
 # ============================================
